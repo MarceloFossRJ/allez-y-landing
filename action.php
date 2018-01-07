43b0f30566be5b8d9759f8ed97ad51612d5950f9
marcelofossrj <marcelo.foss.rj@gmail.com>
2018-01-07 12:02:14 -0200

    ajax submit

--- action.php
+++ action.php
@@ -1,45 +1,47 @@
 <?php
-	$result = json_decode( mailchimp_subscriber_status($_POST['email'], 'subscribed'));
-	// print_r( $result );
-	if( $result->status == 400 ){
-		foreach( $result->errors as $error ) {
-			echo '<p>Error: ' . $error->message . '</p>';
-		}
-	} elseif( $result->status == 'subscribed' ){
-		echo 'Thank you, ' . $result->merge_fields->FNAME . '. You have subscribed successfully';
-	}
-
-
-function mailchimp_subscriber_status( $email, $status){
-
+session_start();
+$email = $_POST['email'];
+$status = 'subscribed';
+if (!empty($email)) {
+  $data = array(
+    'apikey'        => $apikey,
+    'email_address' => $email,
+    'status'        => $status
+  );
   $config = parse_ini_file('config.ini');
   $apiKey = $config['apiKey'];
   $listID = $config['listID'];
-
-  $data = array(
-		'apikey'        => $api_key,
-    'email_address' => $email,
-		'status'        => $status
-	);
-
   $dataCenter = substr($apiKey,strpos($apiKey,'-')+1);
   $url = 'https://' . $dataCenter . '.api.mailchimp.com/3.0/lists/' . $listID . '/members/' . md5(strtolower($data['email_address']));
-
-	$mch_api = curl_init(); // initialize cURL connection
-
+  $mch_api = curl_init(); // initialize cURL connection
   curl_setopt($mch_api, CURLOPT_URL, $url);
-	curl_setopt($mch_api, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Authorization: Basic '.base64_encode( 'user:'.$api_key )));
-	curl_setopt($mch_api, CURLOPT_USERAGENT, 'PHP-MCAPI/2.0');
-	curl_setopt($mch_api, CURLOPT_RETURNTRANSFER, true); // return the API response
-	curl_setopt($mch_api, CURLOPT_CUSTOMREQUEST, 'PUT'); // method PUT
-	curl_setopt($mch_api, CURLOPT_TIMEOUT, 10);
-	curl_setopt($mch_api, CURLOPT_POST, true);
-	curl_setopt($mch_api, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($mch_api, CURLOPT_POSTFIELDS, json_encode($data) ); // send data in json
-
-	$result = curl_exec($mch_api);
+  curl_setopt($mch_api, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Authorization: Basic '.base64_encode( 'user:'.$apiKey )));
+  curl_setopt($mch_api, CURLOPT_USERAGENT, 'PHP-MCAPI/2.0');
+  curl_setopt($mch_api, CURLOPT_RETURNTRANSFER, true); // return the API response
+  curl_setopt($mch_api, CURLOPT_CUSTOMREQUEST, 'PUT'); // method PUT
+  curl_setopt($mch_api, CURLOPT_TIMEOUT, 10);
+  curl_setopt($mch_api, CURLOPT_POST, true);
+  curl_setopt($mch_api, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($mch_api, CURLOPT_POSTFIELDS, json_encode($data) ); // send data in json
+  $result = curl_exec($mch_api);
+  $httpCode = curl_getinfo($mch_api, CURLINFO_HTTP_CODE);
+  if ($httpCode == 200) {
+    $_SESSION['msg'] = '<p style="color: #e87658">You have successfully subscribed to Allez-y Brewing.</p>';
+  }
+  else {
+    switch ($httpCode) {
+      case 214:
+        $msg = 'You are already subscribed.';
+        break;
+      default:
+        $msg = 'Some problem occurred, please try again.';
+        break;
+      }
+    $_SESSION['msg'] = '<p style="color: #e87658">'.$msg.'</p>';
+  }
   curl_close($mch_api);
-	return $result;
 }
-
+// redirect to homepage
+//
+//header('location:index.php');
 ?>
